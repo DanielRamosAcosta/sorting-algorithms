8435542b7c588085bcf7328f4d39867fc0213949
Daniel Ramos <danielramosacosta@hotmail.com>
2015-06-09 21:15:37 +0100

    Done bubble sort

--- include/algorithms.hpp
+++ include/algorithms.hpp
@@ -17,117 +17,91 @@ namespace dra{
 	namespace sort{
 
 		template<typename T>
-		void binary(std::vector<T>& v)
+		void binary(std::vector<T>& vec)
 		{
-			for(std::size_t i = 1; i < v.size(); i++){
-				#ifdef _DEBUG
-					std::cout << "i = " << i << "; " << v << std::endl;
-				#endif
+			for(std::size_t i = 1; i < vec.size(); i++){
 
 				signed_int beg = 0;
 				signed_int end = i - 1;
 				while (beg <= end){
 					std::size_t med = (beg+end)/2;
-					if(v[med] < v[i])
+					if(vec[med] < vec[i])
 						beg = med+1;
 					else
 						end = med-1;
 				}
 
-				v.insert(v.begin() + beg, v[i]);
-				v.erase(v.begin() + i + 1);
-
-				#ifdef _DEBUG
-					std::cout << "i = " << i << "; " << v << "✓" << std::endl;
-				#endif
+				vec.insert(vec.begin() + beg, vec[i]);
+				vec.erase(vec.begin() + i + 1);
 			}
-			#ifdef _DEBUG
-				std::cout << v << "✔" << std::endl;
-			#endif
 		}
 
 		template<typename T>
-		void bubble(std::vector<T>& v)
+		void bubble(std::vector<T>& vec)
 		{
-			
+			for(std::size_t i = 1; i < vec.size(); i++)
+				for(std::size_t j = vec.size()-1; j >= i; j--)
+					if( vec[j] < vec[j-1])
+						std::swap(vec[j-1], vec[j]);
 		}
 
 		template<typename T>
-		void heap(std::vector<T>& v)
+		void heap(std::vector<T>& vec)
 		{
 
 		}
 		
 		template<typename T>
-		void insertion(std::vector<T>& v)
+		void insertion(std::vector<T>& vec)
 		{
-			for(std::size_t i = 1, j = 0; i < v.size(); i++, j = i){
-				#ifdef _DEBUG
-					std::cout << "i = " << i << "; " << v << std::endl;
-				#endif
-				while ((j > 0) && (v[i] < v[j-1])) //important (j<0) to be first
+			for(std::size_t i = 1, j = 0; i < vec.size(); i++, j = i){
+				while ((j > 0) && (vec[i] < vec[j-1])) //important (j<0) to be first
 					j--;
 
-				v.insert(v.begin() + j, v[i]);
-				v.erase(v.begin() + i + 1);
-
-				#ifdef _DEBUG
-					std::cout << "i = " << i << "; " << v << "✓" << std::endl;
-				#endif
+				vec.insert(vec.begin() + j, vec[i]);
+				vec.erase(vec.begin() + i + 1);
 			}
-			#ifdef _DEBUG
-				std::cout << v << "✔" << std::endl;
-			#endif
 		}
 
 		template<typename T>
-		void merge(std::vector<T>& v)
+		void merge(std::vector<T>& vec)
 		{
 
 		}
 
 		template<typename T>
-		void quick(std::vector<T>& v)
+		void quick(std::vector<T>& vec)
 		{
 
 		}
 
 		template<typename T>
-		void radix(std::vector<T>& v)
+		void radix(std::vector<T>& vec)
 		{
 
 		}
 
 		template<typename T>
-		void selection(std::vector<T>& v)
+		void selection(std::vector<T>& vec)
 		{
-			for(std::size_t i = 0; i < v.size(); i++){
-				#ifdef _DEBUG
-					std::cout << "i = " << i << "; " << v << std::endl;
-				#endif
+			for(std::size_t i = 0; i < vec.size(); i++){
 				std::size_t min = i;
-				for(std::size_t j = i + 1; j < v.size(); j++){
-					if(v[j] < v[min])
+				for(std::size_t j = i + 1; j < vec.size(); j++)
+					if(vec[j] < vec[min])
 						min = j;
-				}
-				std::swap(v[i], v[min]);
-				#ifdef _DEBUG
-					std::cout << "i = " << i << "; " << v << "✓" << std::endl;
-				#endif
+	
+				std::swap(vec[i], vec[min]);
 			}
-			#ifdef _DEBUG
-				std::cout << v << "✔" << std::endl;
-			#endif
 		}
 
 		template<typename T>
-		void shake(std::vector<T>& v)
+		void shake(std::vector<T>& vec)
 		{
 
 		}
 
 		template<typename T>
-		void shell(std::vector<T>& v)
+		void shell(std::vector<T>& vec)
 		{
 
 		}
